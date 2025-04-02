46e3dbf6dade9f31f9af87f5b5b2e6e5e875701b
Sidney Aulakh <sidneyaulakh@gmail.com>
2025-04-02 15:09:42 +1100

    mongodb atlas connection

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
@@ -10,6 +14,12 @@ app.set('layout', 'layouts/layout');
 
 app.use(expressLayouts);
 app.use(express.static("public"));
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to MongoDB atlas Database"));
 
 app.use('/', indexRouter);
 
