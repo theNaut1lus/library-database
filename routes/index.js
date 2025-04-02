60d3007c22f8bdf01dcc6a3c6b6c77bcbfb23ab2
Sidney Aulakh <sidneyaulakh@gmail.com>
2025-04-02 14:43:50 +1100

    dir structure, views,layouts and routes

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+//we will set up our routes here
+
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.render("index");
+});
+
+module.exports = router;
